cd7c438d909b616cedb8dad1987b0ddb2189f285
nedim-m <73715705+nedim-m@users.noreply.github.com>
2023-11-14 08:38:25 +0100

    added login page

---
+++ wellness/UI/mobile/lib/screens/login_page.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+class LoginPageView extends StatefulWidget {
+  const LoginPageView({super.key});
+
+  @override
+  State<LoginPageView> createState() => _LoginPageViewState();
+}
+
+class _LoginPageViewState extends State<LoginPageView> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _loginFailed = false;
+  final String _errorMessage = "Invalid username or password!";
+
+  Future<void> _login() async {
+    final String username = _usernameController.text;
+    final String password = _passwordController.text;
+    // Replace the following line with your actual login logic
+    // final response = await _userProvider.login(username, password);
+
+    // Temporary code to simulate login success
+    final response = true;
+
+    if (response) {
+      setState(() {
+        _usernameController.clear();
+        _passwordController.clear();
+      });
+      _loginFailed = false;
+      // ignore: use_build_context_synchronously
+      /* Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomepageView()),
+      );*/
+    } else {
+      setState(() {
+        _loginFailed = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double fontSize = MediaQuery.of(context).size.width * 0.05;
+
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            constraints: const BoxConstraints(maxWidth: 400),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.spa,
+                  size: 100.0,
+                  color: Colors.green[300],
+                ),
+                const SizedBox(height: 16.0),
+                const Text(
+                  'Wellness center - Health',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const SizedBox(height: 32.0),
+                Container(
+                  decoration: BoxDecoration(
+                    border: _loginFailed
+                        ? Border.all(color: Colors.red)
+                        : Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                Container(
+                  decoration: BoxDecoration(
+                    border: _loginFailed
+                        ? Border.all(color: Colors.red)
+                        : Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                if (_loginFailed)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      _errorMessage,
+                      style: const TextStyle(color: Colors.red),
+                    ),
+                  ),
+                const SizedBox(height: 24.0),
+                ElevatedButton(
+                  onPressed: _login,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: fontSize,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

a80c165396d141f58bbc8781c4e5b851561ec57f
nedim-m <73715705+nedim-m@users.noreply.github.com>
2023-12-03 22:09:23 +0100

    fix

---
+++ wellness/UI/mobile/lib/models/treatment.dart
@@ -0,0 +1,22 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'treatment.g.dart';
+
+@JsonSerializable()
+class Treatment {
+  final int id;
+  final String treatmentType;
+  final String category;
+  final String description;
+  final double price;
+  final int duration;
+  final String? picture;
+
+  factory Treatment.fromJson(Map<String, dynamic> json) =>
+      _$TreatmentFromJson(json);
+
+  Treatment(this.id, this.treatmentType, this.category, this.description,
+      this.price, this.picture, this.duration);
+
+  Map<String, dynamic> toJson() => _$TreatmentToJson(this);
+}
--- wellness/UI/mobile/lib/screens/treatment_overview_page.dart
+++ wellness/UI/mobile/lib/screens/treatment_overview_page.dart
@@ -9,10 +9,123 @@ class TreatmentOverview extends StatefulWidget {
 }
 
 class _TreatmentOverviewState extends State<TreatmentOverview> {
+  List<String> filterData = ['Treatment 1', 'Treatment 2', 'Treatment 3'];
+  List<String> myData = [];
+  String selectedTreatment = '';
+  String selectedCategory = '';
+
+  TextEditingController treatmentTypeController = TextEditingController();
+  TextEditingController categoryController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() {
+    setState(() {
+      myData = List.from(filterData);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(),
+      appBar: const AppBarWidget(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      "Enter treatment type",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  DropdownButton<String>(
+                    value: selectedTreatment,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedTreatment = value!;
+                      });
+                    },
+                    items: filterData.map<DropdownMenuItem<String>>(
+                      (String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      },
+                    ).toList(),
+                  ),
+                  const SizedBox(height: 15),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      "Enter category",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  DropdownButton<String>(
+                    value: selectedCategory,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedCategory = value!;
+                      });
+                    },
+                    items: filterData.map<DropdownMenuItem<String>>(
+                      (String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      },
+                    ).toList(),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                columns: const [
+                  DataColumn(
+                    label: Text(
+                      "Treatment Type",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ],
+                rows: myData
+                    .map(
+                      (data) => DataRow(
+                        cells: [
+                          DataCell(Text(data)),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

9ddcce85a4c7558487b05c806bc7e9c31cbde2c3
nedim-m <73715705+nedim-m@users.noreply.github.com>
2023-12-15 20:20:43 +0100

    Update color_coded_calendar.dart
    design done

--- wellness/UI/mobile/lib/widgets/color_coded_calendar.dart
+++ wellness/UI/mobile/lib/widgets/color_coded_calendar.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class ColorCodedCalendar extends StatelessWidget {
-  const ColorCodedCalendar({Key? key});
+  const ColorCodedCalendar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Column(
       children: [
         CalendarHeader(),
+        Gap(5),
         CalendarGrid(),
       ],
     );
@@ -15,7 +17,9 @@ class ColorCodedCalendar extends StatelessWidget {
 }
 
 class CalendarHeader extends StatelessWidget {
-  const CalendarHeader({Key? key});
+  const CalendarHeader({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -56,39 +60,74 @@ class CalendarHeader extends StatelessWidget {
 }
 
 class CalendarGrid extends StatelessWidget {
-  const CalendarGrid({Key? key});
+  const CalendarGrid({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Column(
-              children: [
-                for (int hour = 9; hour <= 19; hour++)
-                  Center(
-                    child: Text('$hour:00'),
-                  ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Column(
-              children: [
-                for (int row = 0; row <= 9; row++)
-                  Center(
-                    child: Container(
+    return Column(
+      children: [
+        for (int hour = 9; hour <= 19; hour++)
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Center(
+                  child: Text('$hour:00'),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Center(
+                  child: Container(
+                    height: 30,
+                    decoration: const BoxDecoration(
                       color: Colors.red,
                     ),
                   ),
-              ],
-            ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        const Gap(15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildLegendSquare(Colors.red),
+            const SizedBox(width: 8),
+            _buildLegendSquare(Colors.green),
+            const SizedBox(width: 8),
+            _buildLegendSquare(Colors.yellow),
+          ],
+        ),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text('Zauzeto'),
+            SizedBox(width: 8),
+            Text('Slobodno'),
+            SizedBox(width: 8),
+            Text('Pauza'),
+          ],
+        ),
+        const Gap(10),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text("Rezervisi"),
+          ),
+        ),
+      ],
     );
   }
+}
+
+Widget _buildLegendSquare(Color color) {
+  return Container(
+    width: 20,
+    height: 20,
+    decoration: BoxDecoration(
+      color: color,
+    ),
+  );
 }
